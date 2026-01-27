17677c5df27f98f7c5c6c185f0c828305b837560
Santosh_Chourasiya <santoshchourasiya1332@gmail.com>
2026-01-28 01:13:31 +0530

    first commit

--- new/payment-system/src/main/java/com/payment/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // Initialize the application and start payment processing
-        System.out.println("Payment System Application Started");
-        // Additional initialization code can be added here
-    }
-}
