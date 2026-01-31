0fb7834fba216afe20fd90c6c8ea90392a40cbd6
Santosh_Chourasiya <santoshchourasiya1332@gmail.com>
2026-01-31 21:54:16 +0530

    new file created and deleted new_java.java file

---
+++ LearningUpdate.java
@@ -0,0 +1,12 @@
+public class LearningUpdate {
+
+    public static void main(String[] args) {
+        LearningUpdate obj = new LearningUpdate();
+        obj.sendJenkinsLearningUpdate();
+    }
+
+    public void sendJenkinsLearningUpdate() {
+        System.out.println("Today, I learned how to create a Jenkins pipeline using Groovy scripts to run code from GitHub.");
+        System.out.println("I also explored how Jenkins pipelines work in different stages and how to write Groovy scripts effectively.");
+    }
+}
